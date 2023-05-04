94b765e795e6c2c2f9c57b43cded9d6a47ef5a28
Alexey <chuzhinov-kr@yandex.ru>
2023-05-04 21:31:21 +0300

    add solution for task 53,54,55,56,57,58,59,60,61

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит
+//  по убыванию элементы каждой строки двумерного массива.
+
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Заполняем массив случайными числами
+void Gen2DArray(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+
+// Печать двумерного массива
+void Print2DArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+    // Функция сортировки элементов в строке двумерного массива, по убыванию
+void SortArr(int[,] array)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+
+int m = ReadData("Введите количество строк M: ");
+int n = ReadData("Введите количество столбцов N: ");
+int[,] matrix = new int[m, n];
+Gen2DArray(matrix,1,100);
+Print2DArray(matrix);
+SortArr(matrix);
+Console.WriteLine();
+Print2DArray(matrix);
---
+++ Sem8Task61/Program.cs
@@ -0,0 +1,42 @@
+﻿// Вывести первые N строк треугольника Паскаля.
+// Сделать вывод в виде равнобедренного
+// треугольника.
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+long Factor(int n)
+{
+    long res = 1;
+    for(int i=1;i<=n;i++)
+    {
+        res*=i;
+    }
+    return res;
+}
+
+void PrintPascalTriangle(int nRaw)
+{
+    for(int i=0;i<nRaw;i++)
+    {
+        for(int k = 0;k<nRaw-i;k++)
+        {
+            Console.Write(" ");
+        }
+        for(int j = 0; j<=i;j++)
+        {
+            Console.Write(" ");
+            Console.Write(Factor(i)/(Factor(j)*Factor(i-j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+int countRaw = ReadData("Введите количество строк Треугольника Паскаля: ");
+PrintPascalTriangle(countRaw);
