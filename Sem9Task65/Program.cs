6b1ca523de802e2db2eb717c82000169a9140d27
Alexey <chuzhinov-kr@yandex.ru>
2023-05-09 17:52:41 +0300

    add solution for task 63,64,65,66,67,68,69

---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. Напишите 
+// программу, которая найдёт все натуральные элементы
+//  в промежутке от M до N.
+
+
+//Метод ввода
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+void FromMToN(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(n + " ");
+        FromMToN(m, n - 1);
+    }
+    else
+    {
+        return;
+    }
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+if (m>n)
+{
+  int buf = m;
+  m = n;
+  m = buf;
+}
+FromMToN(m, n);
+
+
